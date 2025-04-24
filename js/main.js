5aa844c19c0a3672590907f1a46e66021b120058
Shawn Hank <shawn@shawnhank.com>
2025-04-24 16:02:32 -0600

    game complete

--- js/main.js
+++ js/main.js
@@ -19,6 +19,7 @@ const markerEls = [...document.querySelectorAll('#markers > div')];  // Convert 
 
 /*----- event listeners -----*/
 document.getElementById('markers').addEventListener('click', handleDrop);
+playAgainBtn.addEventListener('click', init);
 
 /*----- functions -----*/
 init();
@@ -54,19 +55,66 @@ function handleDrop(evt) {
   const colArr = board[colIdx];
   // 4) Determine the index of the first available "cell" (first `null` element in `colArr`).
   const rowIdx = colArr.indexOf(null);
-  console.log(rowIdx);
   // 5) Update the "cell" in `colArr` with whose turn it is.
   colArr[rowIdx] = turn;
   // 6) Compute and update the state of the game (winner ?).
-  winner = getWinner();
+  winner = getWinner(colIdx, rowIdx);
   // 7) Update whose turn it is.
   turn *= -1;
   // 8) All state has been updated - call render()!
   render();
 }
 
-function getWinner() {
-  return null;
+function getWinner(colIdx, rowIdx) {
+  return checkVertical(colIdx, rowIdx) || checkHorizontal(colIdx, rowIdx) ||
+    checkBackslash(colIdx, rowIdx) || checkForwardslash(colIdx, rowIdx) ||
+    checkTie();
+}
+
+function checkTie() {
+  for (let colArr of board) {
+    if (colArr.includes(null)) return null;
+  }
+  return 'Tie';
+}
+
+function checkForwardslash(colIdx, rowIdx) {
+  const numLeft = countAdjacent(colIdx, rowIdx, -1, -1);
+  const numRight = countAdjacent(colIdx, rowIdx, 1, 1);
+  return numLeft + numRight >= 3 ? turn : null;
+}
+
+function checkBackslash(colIdx, rowIdx) {
+  const numLeft = countAdjacent(colIdx, rowIdx, -1, 1);
+  const numRight = countAdjacent(colIdx, rowIdx, 1, -1);
+  return numLeft + numRight >= 3 ? turn : null;
+}
+
+function checkHorizontal(colIdx, rowIdx) {
+  const numLeft = countAdjacent(colIdx, rowIdx, -1, 0);
+  const numRight = countAdjacent(colIdx, rowIdx, 1, 0);
+  return numLeft + numRight >= 3 ? turn : null;
+}
+
+function checkVertical(colIdx, rowIdx) {
+  const numBelow = countAdjacent(colIdx, rowIdx, 0, -1);
+  return numBelow === 3 ? turn : null;
+}
+
+// col/rowDelta represents the value that col/rowIdx
+// will change by after each iteration
+function countAdjacent(colIdx, rowIdx, colDelta, rowDelta) {
+  let count = 0;
+  colIdx += colDelta;
+  rowIdx += rowDelta;
+  // Use a while loop when you don't know
+  // how many times you need to loop/iterate
+  while (board[colIdx] && board[colIdx][rowIdx] === turn) {
+    count++;
+    colIdx += colDelta;
+    rowIdx += rowDelta;
+  }
+  return count;
 }
 
 // The purpose of the render() function is to 
@@ -82,6 +130,10 @@ function renderControls() {
   // <conditional exp> ? <truthy exp> : <falsy exp>
   playAgainBtn.style.visibility = winner ? 'visible' : 'hidden';
   // TODO: conditionally render the markers
+  markerEls.forEach((markerEl, colIdx) => {
+    const showMarker = board[colIdx].includes(null) && !winner;
+    markerEl.style.visibility = showMarker ? 'visible' : 'hidden';
+  });
 }
 
 function renderMessage() {
